f76cb51b08ac59e1a0a62c78a36fd99f69f22ef0
EidanGar <eidangarcia787i@gmail.com>
2023-07-17 16:34:26 -0400

    Updated next.config.json file to use unsplash img

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["images.unsplash.com", "assets.example.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.example.com",
+        port: "",
+        pathname: "/account123/**",
+      },
+    ],
+  },
+};
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -66,10 +66,10 @@ const Header: FC = () => {
               <span className="sr-only">Menu</span>
               <Image
                 alt="User image"
-                src={
-                  "https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                }
-                className="h-10 w-10 rounded-full object-cover"
+                src={"https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"}
+                className="rounded-full object-cover"
+                width={40}
+                height={40}
               />
 
               <p className="ms-2 hidden text-left text-xs sm:block">
