a2ff7f966efc2f2599f902405d62f4ae078211a9
Eidan Garcia <113457465+EidanGar@users.noreply.github.com>
2023-07-22 21:53:11 +0000

    Cleaned up the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,28 +2,27 @@ import { FC } from "react";
 import Image from "next/image";
 import Icons from "./Icons";
 import Link from "next/link";
+import Button, { buttonVariants } from "@/ui/Button";
+import ThemeToggle from "@/components/ThemeToggle";
 
 const Header: FC = () => {
-  const headerButtonClass =
-    "block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm dark:bg-slate-700 dark:text-slate-50 block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm dark:bg-slate-700 dark:text-slate-50";
-
   return (
     <header className="bg-gray-50 dark:bg-gray-800 fixed top-0 left-0 right-0">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-        <div className="flex items-center sm:justify-between sm:gap-4">
-          <span className={headerButtonClass}>
+      <div className="mx-auto max-w-screen-xl px-4 py-5 sm:px-6 lg:px-8">
+        <div className="flex items-center 1hc:justify-between gap-4">
+          <Button variant="header" size="header">
             <span className="sr-only">Sidebar menu trigger</span>
             <Icons.Menu className="w-5 h-5" />
-          </span>
+          </Button>
 
-          <div className="relative hidden sm:block">
+          <div className="relative hidden 1hc:block">
             <label className="sr-only" htmlFor="search">
               {" "}
               Search{" "}
             </label>
 
             <input
-              className="h-10 w-full rounded-lg border-none bg-white pe-10 ps-4 text-sm shadow-sm outline-none dark:bg-slate-600 dark:text-slate-50 dark:placeholder:text-slate-300 sm:w-56"
+              className="h-10 w-full rounded-lg border-none bg-white pe-10 ps-4 text-sm shadow-sm outline-none dark:bg-slate-600 dark:text-slate-50 dark:placeholder:text-slate-300 1hc:w-56"
               id="search"
               type="text"
               placeholder="Search website..."
@@ -38,25 +37,30 @@ const Header: FC = () => {
             </button>
           </div>
 
-          <div className="flex flex-1 items-center justify-between gap-8 sm:justify-end">
+          <div className="flex flex-1 items-center justify-between gap-8 1hc:justify-end">
             <div className="flex gap-4">
-              <button
+              <Button
                 type="button"
-                className="block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700 sm:hidden"
+                variant="header"
+                size="header"
+                className="1hc:hidden"
               >
-                <span className="sr-only">Search</span>
+                <span className="sr-only">Search Modal Trigger</span>
                 <Icons.Search className="w-5 h-5" />
-              </button>
+              </Button>
 
-              <Link href="/projects/new" className={headerButtonClass}>
+              <Link
+                href="/projects/new"
+                className={buttonVariants({
+                  variant: "header",
+                  size: "header",
+                })}
+              >
                 <span className="sr-only">New Project</span>
                 <Icons.FolderAdd className="w-5 h-5" />
               </Link>
 
-              <Link href="/notifications" className={headerButtonClass}>
-                <span className="sr-only">Notifications</span>
-                <Icons.Bell className="w-5 h-5" />
-              </Link>
+              <ThemeToggle />
             </div>
 
             <button
@@ -66,7 +70,7 @@ const Header: FC = () => {
               <span className="sr-only">Menu</span>
               <Image
                 alt="User image"
-                src={"https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"}
+                src={"https://i.postimg.cc/j5CS5hLY/example-User-Image.webp"}
                 className="rounded-full object-cover"
                 width={40}
                 height={40}
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,7 +1,12 @@
 import { LiaLaptopSolid } from "react-icons/lia";
 import { LuMoon } from "react-icons/lu";
 import { HiOutlineSun } from "react-icons/hi";
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import {
+  BsChevronLeft,
+  BsChevronRight,
+  BsCircle,
+  BsCheckLg,
+} from "react-icons/bs";
 import { BiChevronDown } from "react-icons/bi";
 import { AiOutlineBell } from "react-icons/ai";
 import { TbFolderPlus } from "react-icons/tb";
@@ -19,6 +24,8 @@ export const Icons = {
   FolderAdd: TbFolderPlus,
   Search: GoSearch,
   Menu: FiMenu,
+  Circle: BsCircle,
+  Check: BsCheckLg,
 };
 
 export default Icons;
